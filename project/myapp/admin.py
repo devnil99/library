8a2204af3bf2e60be167c6592456d50d03f34859
devnil99 <nilkamalshriwas91@gmail.com>
2025-07-03 20:38:55 +0530

    update
    update

--- project/myapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(AdminRegister)
-admin.site.register(UserRegister)
-admin.site.register(Book)
-admin.site.register(BookRecord)
